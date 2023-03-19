8d5d25b6f7de1b7781138348a699581712d7be40
Dmitri Mandaji <badmopss@gmail.com>
2023-03-19 19:30:39 +0100

    add the String toString

---
+++ src/Home/apartmentHome.java
@@ -0,0 +1,22 @@
+package Home;
+
+public class apartmentHome extends Home {
+  private int entrance;
+  private String address;
+  public apartmentHome(String address, int entrance ) {
+    super(address);
+    this.entrance = entrance;
+  }
+
+  public int getEntrance() {
+    return entrance;
+  }
+
+  public void setEntrance(int entrance) {
+    this.entrance = entrance;
+  }
+  @Override
+  public String toString() {
+    return "Apartment house at " + getAddress();
+  }
+}
--- src/Home/privateHome.java
+++ src/Home/privateHome.java
@@ -1,4 +1,12 @@
 package Home;
 
 public class privateHome extends Home {
+  public privateHome(String address) {
+    super(address);
+  }
+
+  @Override
+  public String toString() {
+    return "Private house at " + getAddress();
+  }
 }
