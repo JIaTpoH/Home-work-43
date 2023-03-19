dc38b1a16796f09c8dc2d672305c34e8af415e3a
Dmitri Mandaji <badmopss@gmail.com>
2023-03-19 20:51:10 +0100

    work on mistakes part 2

--- src/Home/apartmentHome.java
+++ src/Home/apartmentHome.java
@@ -1,17 +1,9 @@
 package Home;
 
 public class apartmentHome extends Home {
-  private int entrance;
+  private final int entrance;
   public apartmentHome(String address, int entrance ) {
     super(address);
-    this.entrance = entrance;
-  }
-
-  public int getEntrance() {
-    return entrance;
-  }
-
-  public void setEntrance(int entrance) {
     this.entrance = entrance;
   }
   @Override
--- src/Home/mainHome.java
+++ src/Home/mainHome.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class mainHome {
   public static void main(String[] args)  {
-    List<Home> houses = new ArrayList<Home>();
-    privateHome house1 = new privateHome("Thomass-Mann-str. 2");
+    List<Home> houses = new ArrayList<>();
+    privateHome house1 = new privateHome("Thomas-Mann-str. 2");
     privateHome house2 = new privateHome("Heussenstamstr. 1");
     apartmentHome house3 = new apartmentHome("Danziger Platz 12 ",2);
 
