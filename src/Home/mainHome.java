3b4fe7620ef86a29bc02366356b81e7464b32276
Dmitri Mandaji <badmopss@gmail.com>
2023-03-19 21:26:13 +0100

    add the new demolish method in class mainHome

--- src/Home/mainHome.java
+++ src/Home/mainHome.java
@@ -5,15 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class mainHome {
-  public static void main(String[] args)  {
+  public static void main(String[] args) {
     List<Home> houses = new ArrayList<>();
     privateHome house1 = new privateHome("Thomas-Mann-str. 2");
     privateHome house2 = new privateHome("Heussenstamstr. 1");
-    apartmentHome house3 = new apartmentHome("Danziger Platz 12 ",2);
+    apartmentHome house3 = new apartmentHome("Danziger Platz 12 ", 2);
 
     houses.add(house1);
     houses.add(house2);
     houses.add(house3);
+
+
+    house1.demolish();
+    house3.demolish();
+    System.out.println();
+
 
     for (Home house : houses) {
       System.out.println(house);
